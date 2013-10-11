bcffe016f6d1542cfc8996fd97e6baefa9b7801a
Brandon Tilley <brandon@brandontilley.com>
2013-10-11 11:15:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moviesearch::Application.config.secret_key_base = '36a9613b2bebee147fd21689c8cdde4e8d019349f358c65a1d29e8e36cf2af3c638d98c3ad6f2433d2d86c8fb11d8169e40a868ca4d5c76f30041babb93582ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moviesearch::Application.config.session_store :cookie_store, key: '_moviesearch_session'

4604d6bbbe93232043def7c240ab81d379191ec5
Brandon Tilley <brandon@brandontilley.com>
2013-10-25 13:22:00 -0700

    Use env variable for moviedb API key

--- app/models/api_query.rb
+++ app/models/api_query.rb
@@ -3,10 +3,10 @@ class ApiQuery
   base_uri 'api.themoviedb.org'
 
   def top_rated(page=1)
-    self.class.get("/3/movie/top_rated?page=#{page}&api_key=#{API_KEY}")
+    self.class.get("/3/movie/top_rated?page=#{page}&api_key=#{ENV['MOVIEDB_API_KEY']}")
   end
 
   def movie(id)
-    self.class.get("/3/movie/#{id}?append_to_response=casts&api_key=#{API_KEY}")
+    self.class.get("/3/movie/#{id}?append_to_response=casts&api_key=#{ENV['MOVIEDB_API_KEY']}")
   end
 end
